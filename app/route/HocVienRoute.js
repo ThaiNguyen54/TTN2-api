8f1a19288f9ac121ad08f6dc0ea2f8acf81981e2
ThaiNguyen54 <19522188@gm.uit.edu.vn>
2024-08-01 14:22:50 +0700

    modify HocVien

--- app/route/HocVienRoute.js
+++ app/route/HocVienRoute.js
@@ -9,6 +9,6 @@ const router = express.Router()
 router.post('/ttn2/v1/auth/hocvien', HocVienController.AddHocVien);
 router.get('/ttn2/v1/auth/hocvien', HocVienController.GetAllHocVien);
 router.get('/ttn2/v1/auth/count-cn/hocvien', HocVienController.FindAllAndContCNHocVien);
-router.put('/ttn2/v1/auth/hocvien/:cccd', HocVienController.UpdateHocVien);
+router.put('/ttn2/v1/auth/hocvien/:MaHocVien', HocVienController.UpdateHocVien);
 router.delete('/ttn2/v1/auth/hocvien/:MaHocVien', HocVienController.DeleteHocVien);
 export default router
